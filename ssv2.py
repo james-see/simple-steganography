75e52609a04e9f3242e92beb3d3a8513a9772f7b
James Campbell <james@jamescampbell.us>
2021-02-15 23:01:21 -0500

    clean up old files and added self-commented code

--- ssv2.py
+++ ssv2.py
@@ -2,13 +2,20 @@
 from PIL import Image
 import stepic
 
-imager = input('full path of image location: ')  # if using python 3.x
+# get image path (use pngs/original.png)
+imager = input('full path of image location: ') 
+# open image
 im = Image.open(imager)
+# get data to encode and hide
 encodeddata = input('message to hide: ').encode()
 # TODO add ability to also ask for a file to hide
 # datatoencode = open('test.txt', 'rb').read()
+# hide the data in the image
 im2 = stepic.encode(im, encodeddata)
+# save the result
 im2.save('stegtest.png', "PNG")
+# open the result
 im1 = Image.open('stegtest.png')
+# decode and print the message
 s = stepic.decode(im1)
 print(f"The message you hid in the image: {s}")
