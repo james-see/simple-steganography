9f083e95de7b1578cd76f541e2daf151cc5a7f03
James Campbell <james@jamescampbell.us>
2021-02-15 22:23:06 -0500

    Committing fixes in the dark, seriously, who killed my power!?

--- ssv2.py
+++ ssv2.py
@@ -2,19 +2,12 @@
 from PIL import Image
 import stepic
 
-#for ch in "hello world!":
-#    d = ord(ch)
-#    b = bin(d)
-#    print(ch, d, b)
-try:
-	imager = raw_input('full path of image location: ')
-except:
-	imager = input('full path of image location: ') # if using python 3.x
-im = Image.open (imager)
+imager = input('full path of image location: ')  # if using python 3.x
+im = Image.open(imager)
 im2 = stepic.encode(im, 'HELLO')
-im2.save('stegtest.png',"PNG")
+im2.save('stegtest.png', "PNG")
 im1 = Image.open('stegtest.png')
-s = stepic.decode(im1) 
-print (s)
-data = s.decode() 
-print (data)
+s = stepic.decode(im1)
+print(s)
+data = s.decode()
+print(data)
